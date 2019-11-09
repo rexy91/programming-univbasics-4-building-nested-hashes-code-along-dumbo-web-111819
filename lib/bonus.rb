c7ec10b585213516687e4422eb076ccd871c1c16
Yu Hao Ye <rxye1991@gmail.com>
2019-11-09 23:21:58 +0000

    Done.

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -18,11 +18,9 @@ def bonus
           {name: "Nurse", age: "44", attitude: "worried"}
       ]
    }
-  }
-
-
   
-
+  epic_tragedy[:montague][:hero][:status] = 'dead'
+  epic_tragedy[:capulet][:heroine][:status] = 'dead'
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
 end
